371610e54ba57859dc04f31c23cc9fb5bb91f985
Batuhan Orhan <bthnorhan00@gmail.com>
2017-09-19 16:29:41 +0300

    SplashScreen
    SplashScreen oluşturuldu.
    Başlangıç activitysi olarak ayarlandı.

---
+++ Project/app/src/main/java/com/inwhiter/inviteapp/project/ActivityB/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.inwhiter.inviteapp.project.ActivityB;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.inwhiter.inviteapp.project.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
