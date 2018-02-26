f3656b6268f2b2de3605eff96a63b87b590fb19c
gamze <gamzeezmag@users.noreply.github.com>
2018-02-26 19:31:15 +0300

    ödeme tamam
    bütün paketler için ödeme eklendi

--- Project/app/src/main/java/com/inwhiter/inviteapp/project/Fragment/Fragments/PurchaseFragment.java
+++ Project/app/src/main/java/com/inwhiter/inviteapp/project/Fragment/Fragments/PurchaseFragment.java
@@ -23,29 +23,158 @@ import com.inwhiter.inviteapp.project.Utils.Inventory;
 import com.inwhiter.inviteapp.project.Utils.Purchase;
 
 
-
 public class PurchaseFragment extends BaseFragment implements IabBroadcastListener {
 
+    // Debug tag, for logging
+    static final String TAG = "InwhiterPurchase";
+    static final String ADET_50 = "davetiye50";
+    static final String ADET_100 = "davetiye100";
+    static final String ADET_500 = "davetiye500";
+    static final String ADET_10 = "davetiye10";
+    static final int RC_REQUEST = 9900;
     Button purchase10;
     Button purchase50;
     Button purchase100;
     Button purchase500;
     IInAppBillingService mService;
     ServiceConnection mServiceConn;
-    // Debug tag, for logging
-    static final String TAG = "InwhiterPurchase";
     IabHelper mHelper;
-
     // Provides purchase notification while this app is running
     IabBroadcastReceiver mBroadcastReceiver;
-    static final String ADET_50="davetiye50";
-    static final String ADET_100="davetiye100";
-    static final String ADET_500="davetiye500";
-    static final String ADET_10="davetiye10";
-    static final int RC_REQUEST=9900;
+    int alinanDavetiyeSayisi = 0;
+    // Called when consumption is complete
+    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            Log.d(TAG, "Alım bitti. Atın alma: " + purchase + ", sonuç: " + result);
 
-    int alinanDavetiyeSayisi=0;
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
 
+            // We know this is the "gas" sku because it's the only one we consume,
+            // so we don't check which sku was consumed. If you have more than one
+            // sku, you probably should check...
+            if (result.isSuccess()) {
+                // successfully consumed, so we apply the effects of the item in our
+                // game world's logic, which in our case means filling the gas tank a bit
+                Log.d(TAG, "Alım bitti");
+                if (purchase.getSku().equals(ADET_10)) {
+                    alinanDavetiyeSayisi += 10;
+                } else if (purchase.getSku().equals(ADET_50)) {
+                    alinanDavetiyeSayisi += 50;
+                } else if (purchase.getSku().equals(ADET_100)) {
+                    alinanDavetiyeSayisi += 100;
+                } else if (purchase.getSku().equals(ADET_500)) {
+                    alinanDavetiyeSayisi += 500;
+                }
+                saveData();
+                alert("Toplam devetiye sayısı " + String.valueOf(alinanDavetiyeSayisi));
+            } else {
+                complain("Alım hatası: " + result);
+            }
+            updateUi();
+            setWaitScreen(false);
+            Log.d(TAG, "Alım sonu.");
+        }
+    };
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Envanter sorgusu bitti.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (mHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure()) {
+                complain("Envanter sorgusunda hata: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Envanter sorgusu başarılı.");
+
+
+            Purchase purchase10 = inventory.getPurchase(ADET_10);
+            if (purchase10 != null && verifyDeveloperPayload(purchase10)) {
+                Log.d(TAG, "10 davetiye bulundu");
+                try {
+                    mHelper.consumeAsync(inventory.getPurchase(ADET_10), mConsumeFinishedListener);
+                } catch (IabAsyncInProgressException e) {
+                    complain("10 davetiye paketi sorgulanırken hata oluştu. Asenkron başka bir işlem yapılıyor.");
+                }
+                return;
+            }
+            Purchase purchase50 = inventory.getPurchase(ADET_50);
+            if (purchase50 != null && verifyDeveloperPayload(purchase50)) {
+                Log.d(TAG, "50 davetiye bulundu");
+                try {
+                    mHelper.consumeAsync(inventory.getPurchase(ADET_50), mConsumeFinishedListener);
+                } catch (IabAsyncInProgressException e) {
+                    complain("50 davetiye paketi sorgulanırken hata oluştu. Asenkron başka bir işlem yapılıyor.");
+                }
+                return;
+            }
+            Purchase purchase100 = inventory.getPurchase(ADET_100);
+            if (purchase100 != null && verifyDeveloperPayload(purchase100)) {
+                Log.d(TAG, "100 davetiye bulundu");
+                try {
+                    mHelper.consumeAsync(inventory.getPurchase(ADET_100), mConsumeFinishedListener);
+                } catch (IabAsyncInProgressException e) {
+                    complain("100 davetiye paketi sorgulanırken hata oluştu. Asenkron başka bir işlem yapılıyor.");
+                }
+                return;
+            }
+            Purchase purchase500 = inventory.getPurchase(ADET_500);
+            if (purchase500 != null && verifyDeveloperPayload(purchase500)) {
+                Log.d(TAG, "500 davetiye bulundu");
+                try {
+                    mHelper.consumeAsync(inventory.getPurchase(ADET_500), mConsumeFinishedListener);
+                } catch (IabAsyncInProgressException e) {
+                    complain("500 davetiye paketi sorgulanırken hata oluştu. Asenkron başka bir işlem yapılıyor.");
+                }
+                return;
+            }
+
+
+            updateUi();
+            setWaitScreen(false);
+            Log.d(TAG, "Envanter sorgusu bitti, ekran düzenleniyor.");
+        }
+    };
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Satın alma tamamlandı: " + result + ", purchase: " + purchase);
+
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
+
+            if (result.isFailure()) {
+                complain("Satın alma hatası: " + result);
+                setWaitScreen(false);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                complain("Satın ama hatası. Doğrulama başarısız.");
+                setWaitScreen(false);
+                return;
+            }
+
+            Log.d(TAG, "Satın alma başarılı");
+
+            if (purchase.getSku().equals(ADET_10) || purchase.getSku().equals(ADET_50) || purchase.getSku().equals(ADET_100) || purchase.getSku().equals(ADET_500)) {
+
+                Log.d(TAG, "Davetiye alındı");
+                try {
+                    mHelper.consumeAsync(purchase, mConsumeFinishedListener);
+                } catch (IabAsyncInProgressException e) {
+                    complain("Satın alma sonrası elde edilemedi. Asenkron başka bir işlem var.");
+                    setWaitScreen(false);
+                    return;
+                }
+            }
+
+        }
+    };
 
     public PurchaseFragment() {
     }
@@ -55,14 +184,12 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
         return R.layout.fragment_purchase;
     }
 
-
-
     @Override
     protected void init() {
-        purchase10 = (Button)getActivity().findViewById(R.id.bt_purchase_10);
-        purchase50 = (Button)getActivity().findViewById(R.id.bt_purchase_50);
-        purchase100 = (Button)getActivity().findViewById(R.id.bt_purchase_100);
-        purchase500 = (Button)getActivity().findViewById(R.id.bt_purchase_500);
+        purchase10 = (Button) getActivity().findViewById(R.id.bt_purchase_10);
+        purchase50 = (Button) getActivity().findViewById(R.id.bt_purchase_50);
+        purchase100 = (Button) getActivity().findViewById(R.id.bt_purchase_100);
+        purchase500 = (Button) getActivity().findViewById(R.id.bt_purchase_500);
         mServiceConn = new ServiceConnection() {
             @Override
             public void onServiceDisconnected(ComponentName name) {
@@ -80,7 +207,7 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
         serviceIntent.setPackage("com.android.vending");
         getActivity().bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
 
-        String base64EncodedPublicKey="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg9C8RqdHJwKBNJCPG9T+aAaL/NIr4/5Hsf70V5Z3bTIL/O/JVBCh9EQqXHbZcweMnTptJFxkKLHE0qxG1qkL+8xIeSgux45h/qygfa36vI+ul14IEOP64QZf6ZPWIk+NXEqSrql4XTepGhsM+mllXDvooojURg88lWF7X/pnG5JwjcVph2+Nb6kFJqP3LiSTxpawE4P61tEC8ar/WgwRzaDTq0GuGmbR6Dqdat/b5uM1TEfU84Te37wgyS2+Vja2BFRocMi26rmF4vXGltCD0ahxcZssxPFZW0B+aTC/bV/pO5b+hwBkJbSZ19sCqmoIDpD8oXN4JHNhrOw5f83z7wIDAQAB";
+        String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg9C8RqdHJwKBNJCPG9T+aAaL/NIr4/5Hsf70V5Z3bTIL/O/JVBCh9EQqXHbZcweMnTptJFxkKLHE0qxG1qkL+8xIeSgux45h/qygfa36vI+ul14IEOP64QZf6ZPWIk+NXEqSrql4XTepGhsM+mllXDvooojURg88lWF7X/pnG5JwjcVph2+Nb6kFJqP3LiSTxpawE4P61tEC8ar/WgwRzaDTq0GuGmbR6Dqdat/b5uM1TEfU84Te37wgyS2+Vja2BFRocMi26rmF4vXGltCD0ahxcZssxPFZW0B+aTC/bV/pO5b+hwBkJbSZ19sCqmoIDpD8oXN4JHNhrOw5f83z7wIDAQAB";
         // compute your public key and store it in base64EncodedPublicKey
         mHelper = new IabHelper(getContext(), base64EncodedPublicKey);
 
@@ -124,10 +251,28 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
             }
         });
 
+        purchase10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                davetiyeSatinAl(ADET_10);
+            }
+        });
+        purchase50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                davetiyeSatinAl(ADET_50);
+            }
+        });
         purchase100.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 davetiyeSatinAl(ADET_100);
+            }
+        });
+        purchase500.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                davetiyeSatinAl(ADET_500);
             }
         });
 
@@ -137,40 +282,6 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
     protected void handlers() {
 
     }
-
-    // Listener that's called when we finish querying the items and subscriptions we own
-    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
-            Log.d(TAG, "Envanter sorgusu bitti.");
-
-            // Have we been disposed of in the meantime? If so, quit.
-            if (mHelper == null) return;
-
-            // Is it a failure?
-            if (result.isFailure()) {
-                complain("Envanter sorgusunda hata: " + result);
-                return;
-            }
-
-            Log.d(TAG, "Envanter sorgusu başarılı.");
-
-            // Check for gas delivery -- if we own gas, we should fill up the tank immediately
-            Purchase purchase100 = inventory.getPurchase(ADET_100);
-            if (purchase100 != null && verifyDeveloperPayload(purchase100)) {
-                Log.d(TAG, "100 davetiye bulundu");
-                try {
-                    mHelper.consumeAsync(inventory.getPurchase(ADET_100), mConsumeFinishedListener);
-                } catch (IabAsyncInProgressException e) {
-                    complain("100 davetiye alınırken hata oluştu. Asenkron başka bir işlem yapılıyor.");
-                }
-                return;
-            }
-
-            updateUi();
-            setWaitScreen(false);
-            Log.d(TAG, "Envanter sorgusu bitti, ekran düzenleniyor.");
-        }
-    };
 
     @Override
     public void receivedBroadcast() {
@@ -183,8 +294,6 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
         }
     }
 
-
-
     // User clicked the "Buy Gas" button
     public void davetiyeSatinAl(String paketTuru) {
         Log.d(TAG, "Davetiye satın alma isteği geldi");
@@ -192,7 +301,7 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
         // launch the gas purchase UI flow.
         // We will be notified of completion via mPurchaseFinishedListener
         setWaitScreen(true);
-        Log.d(TAG, paketTuru+" için satın alma işlemi yapılıyor.");
+        Log.d(TAG, paketTuru + " için satın alma işlemi yapılıyor.");
 
         /* TODO: for security, generate your payload here for verification. See the comments on
          *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
@@ -208,8 +317,9 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
         }
     }
 
-
-    /** Verifies the developer payload of a purchase. */
+    /**
+     * Verifies the developer payload of a purchase.
+     */
     boolean verifyDeveloperPayload(Purchase p) {
         String payload = p.getDeveloperPayload();
 
@@ -238,73 +348,6 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
 
         return true;
     }
-
-    // Callback for when a purchase is finished
-    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-            Log.d(TAG, "Satın alma tamamlandı: " + result + ", purchase: " + purchase);
-
-            // if we were disposed of in the meantime, quit.
-            if (mHelper == null) return;
-
-            if (result.isFailure()) {
-            complain("Satın alma hatası: " + result);
-                setWaitScreen(false);
-                return;
-            }
-            if (!verifyDeveloperPayload(purchase)) {
-                complain("Satın ama hatası. Doğrulama başarısız.");
-                setWaitScreen(false);
-                return;
-            }
-
-            Log.d(TAG, "Satın alma başarılı");
-
-            if (purchase.getSku().equals(ADET_100)) {
-
-                Log.d(TAG, "100 davetiye alındı");
-                try {
-                    mHelper.consumeAsync(purchase, mConsumeFinishedListener);
-                } catch (IabAsyncInProgressException e) {
-                    complain("Satın alma sonrası elde edilemedi. Asenkron başka bir işlem var.");
-                    setWaitScreen(false);
-                    return;
-                }
-            }
-
-        }
-    };
-
-    // Called when consumption is complete
-    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
-        public void onConsumeFinished(Purchase purchase, IabResult result) {
-            Log.d(TAG, "Alım bitti. Atın alma: " + purchase + ", sonuç: " + result);
-
-            // if we were disposed of in the meantime, quit.
-            if (mHelper == null) return;
-
-            // We know this is the "gas" sku because it's the only one we consume,
-            // so we don't check which sku was consumed. If you have more than one
-            // sku, you probably should check...
-            if (result.isSuccess()) {
-                // successfully consumed, so we apply the effects of the item in our
-                // game world's logic, which in our case means filling the gas tank a bit
-                Log.d(TAG, "Alım bitti");
-                if (purchase.getSku().equals(ADET_100)) {
-                    alinanDavetiyeSayisi += 100;
-                }
-                saveData();
-                alert("Toplam devetiye sayısı " + String.valueOf(alinanDavetiyeSayisi));
-            }
-            else {
-                complain("Alım hatası: " + result);
-            }
-            updateUi();
-            setWaitScreen(false);
-            Log.d(TAG, "Alım sonu.");
-        }
-    };
-
 
     // We're being destroyed. It's important to dispose of the helper here!
     @Override
@@ -335,7 +378,7 @@ public class PurchaseFragment extends BaseFragment implements IabBroadcastListen
     }
 
     void complain(String message) {
-        Log.e(TAG, "**** TrivialDrive Error: " + message);
+        Log.e(TAG, "**** Inwhiter Error: " + message);
         alert("Error: " + message);
     }
 
